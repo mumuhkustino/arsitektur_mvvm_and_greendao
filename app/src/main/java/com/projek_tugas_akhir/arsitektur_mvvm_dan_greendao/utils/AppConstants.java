ed19f16ef3d5037175b7415fd03237b97cc7edd7
mumuhkustino <mumuhkustino@gmail.com>
2021-04-30 21:01:40 +0700

    Add App Constant JSON

--- app/src/main/java/com/projek_tugas_akhir/arsitektur_mvvm_dan_greendao/utils/AppConstants.java
+++ app/src/main/java/com/projek_tugas_akhir/arsitektur_mvvm_dan_greendao/utils/AppConstants.java
@@ -8,8 +8,14 @@ public final class AppConstants {
 
     public static final String DB_NAME = "mvvm_greendao.db";
 
-    public static final String SEED_DATABASE_MEDICINES = "seed/medicine.json";
-    public static final String SEED_DATABASE_HOSPITALS = "seed/hospital.json";
+    public static final String SEED_DATABASE_MEDICINES_1000 = "seed/medicine_1000k.json";
+    public static final String SEED_DATABASE_MEDICINES_500 = "seed/medicine_500k.json";
+    public static final String SEED_DATABASE_MEDICINES_100 = "seed/medicine_100k.json";
+    public static final String SEED_DATABASE_MEDICINES_10 = "seed/medicine_10k.json";
+    public static final String SEED_DATABASE_HOSPITALS_1000 = "seed/hospital_1000.json";
+    public static final String SEED_DATABASE_HOSPITALS_500 = "seed/hospital_500.json";
+    public static final String SEED_DATABASE_HOSPITALS_100 = "seed/hospital_100.json";
+    public static final String SEED_DATABASE_HOSPITALS_10 = "seed/hospital_10.json";
 
     public static final String TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss";
 
