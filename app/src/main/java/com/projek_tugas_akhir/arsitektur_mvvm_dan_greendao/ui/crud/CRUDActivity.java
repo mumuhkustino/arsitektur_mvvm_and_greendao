6987ea3daf19a9224a62bb55929989c77e32909f
Mumuh Andev <mumuhandev@gmail.com>
2021-06-19 17:28:56 +0700

    Add method for write to excel

--- app/src/main/java/com/projek_tugas_akhir/arsitektur_mvvm_dan_greendao/ui/crud/CRUDActivity.java
+++ app/src/main/java/com/projek_tugas_akhir/arsitektur_mvvm_dan_greendao/ui/crud/CRUDActivity.java
@@ -1,19 +1,36 @@
 package com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.ui.crud;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.material.tabs.TabLayout;
 import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.BR;
 import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.R;
+import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.data.others.ExecutionTimePreference;
 import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.databinding.ActivityCrudBinding;
 import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.di.component.ActivityComponent;
 import com.projek_tugas_akhir.arsitektur_mvvm_dan_greendao.ui.base.BaseActivity;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -23,6 +40,10 @@ public class CRUDActivity extends BaseActivity<ActivityCrudBinding, CRUDViewMode
     CRUDPagerAdapter pagerAdapter;
 
     private ActivityCrudBinding crudBinding;
+    private Toolbar mToolbar;
+    private ExecutionTimePreference executionTimePreference;
+    private String fileName = "/Execution_Time.xls";
+    private File filePath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + fileName);
 
     @Override
     public int getBindingVariable() {
@@ -60,7 +81,73 @@ public class CRUDActivity extends BaseActivity<ActivityCrudBinding, CRUDViewMode
             ((Animatable) drawable).start();
         switch (item.getItemId()) {
             case R.id.action_export:
+                HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+                HSSFSheet hssfSheet = hssfWorkbook.createSheet("Execution Time");
 
+                List<List<String>> listExecutionTime = new ArrayList<>();
+                List<String> dataExecutionTime = new ArrayList<>();
+                dataExecutionTime.add("TYPE CRUD");
+                dataExecutionTime.add("NUMBER OF RECORD");
+                dataExecutionTime.add("TIME DB (MS)");
+                dataExecutionTime.add("TIME VIEW (MS)");
+                dataExecutionTime.add("TIME ALL (MS)");
+                listExecutionTime.add(dataExecutionTime);
+                dataExecutionTime = new ArrayList<>();
+                dataExecutionTime.add("INSERT");
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getNumOfRecordInsert());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getDatabaseInsertTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getViewInsertTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getAllInsertTime());
+                listExecutionTime.add(dataExecutionTime);
+                dataExecutionTime = new ArrayList<>();
+                dataExecutionTime.add("SELECT");
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getNumOfRecordSelect());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getDatabaseSelectTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getViewSelectTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getAllSelectTime());
+                listExecutionTime.add(dataExecutionTime);
+                dataExecutionTime = new ArrayList<>();
+                dataExecutionTime.add("UPDATE");
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getNumOfRecordUpdate());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getDatabaseUpdateTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getViewUpdateTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getAllUpdateTime());
+                listExecutionTime.add(dataExecutionTime);
+                dataExecutionTime = new ArrayList<>();
+                dataExecutionTime.add("DELETE");
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getNumOfRecordDelete());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getDatabaseDeleteTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getViewDeleteTime());
+                dataExecutionTime.add(executionTimePreference.getExecutionTime().getAllDeleteTime());
+                listExecutionTime.add(dataExecutionTime);
+                int rowNum = 0;
+                for (int i = 0; i < listExecutionTime.size(); i++) {
+                    List<String> objArr = listExecutionTime.get(i);
+                    HSSFRow row = hssfSheet.createRow(rowNum++);
+
+                    int cellNum = 0;
+                    for (String obj : objArr) {
+                        Cell cell = row.createCell(cellNum++);
+                        cell.setCellValue(obj);
+                    }
+                }
+
+                try {
+                    if (!filePath.exists()){
+                        filePath.createNewFile();
+                    }
+
+                    FileOutputStream fileOutputStream= new FileOutputStream(filePath);
+                    hssfWorkbook.write(fileOutputStream);
+
+                    if (fileOutputStream!=null){
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                    Log.d("TAG", "onOptionsItemSelected: SAVE FILE SUCCESS");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -68,6 +155,15 @@ public class CRUDActivity extends BaseActivity<ActivityCrudBinding, CRUDViewMode
     }
 
     private void setUp() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                PackageManager.PERMISSION_GRANTED);
+
+        executionTimePreference = new ExecutionTimePreference(this);
+
+        mToolbar = crudBinding.toolbar;
+        setSupportActionBar(mToolbar);
+
         pagerAdapter.setCount(4);
 
         crudBinding.crudViewPager.setAdapter(pagerAdapter);
